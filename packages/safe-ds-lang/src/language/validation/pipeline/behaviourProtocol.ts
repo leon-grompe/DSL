8409638dbd15373c94f78813954dab5552558e37
Leon Grompe <leon.grompe@gmail.com>
2026-04-29 14:00:37 +0200

    changed validation message to be consistent with previous renaming

--- packages/safe-ds-lang/src/language/validation/pipeline/behaviourProtocol.ts
+++ packages/safe-ds-lang/src/language/validation/pipeline/behaviourProtocol.ts
@@ -1,9 +1,9 @@
 import { ValidationAcceptor } from 'langium';
 import { isSdsClass, isSdsFunction, SdsAnnotatedObject, SdsCall, SdsPipeline, SdsPlaceholder } from '../../generated/ast.js';
 import { SafeDsServices } from '../../index.js';
-import { ProtocolBlock, ElementaryBlock, AlternativeBlock, RepetitionBlock, SequenceBlock, Phase } from './model.js';
+import { ProtocolBlock, ElementaryBlock, AlternativeBlock, RepetitionBlock, SequenceBlock, Activity } from './model.js';
 import { ValidationResult, ValidationError} from './validationDataStructures.js'
-import { SafeDsSlicer } from '../../flow/safe-ds-slicer.js'
+
 
 export const CODE_PIPELINE_BEHAVIOUR_PROTOCOL = 'pipeline/behaviour-protocol';
 
@@ -13,7 +13,7 @@ export const pipelineMustFollowBehaviourProtocol = (services: SafeDsServices) =>
 
     return (node: SdsPipeline, accept: ValidationAcceptor) => {
         const calls = [] as SdsCall[];
-        const sequence = [] as Phase[];
+        const sequence = [] as Activity[];
         
         // fill the sequence of found calls in the pipeline
         for (const statement of node.body.statements){
@@ -28,19 +28,20 @@ export const pipelineMustFollowBehaviourProtocol = (services: SafeDsServices) =>
                 if (!callable || !(isSdsFunction(callable) || isSdsClass(callable))) continue;
 
                 const annotation = builtinAnnotations.getDSPipelinePhase(callable as SdsAnnotatedObject);
-                sequence.push(annotation ? new Phase(annotation.name) : new Phase('Any'));
+                sequence.push(annotation ? new Activity(annotation.name) : new Activity('Any'));
             }
         }
 
         const result = behaviourProtocol.validate(sequence, 0);
         
         if (!result.isValid){
-            const validationMessage = computeValidationMessage(result)
+            
 
             const call = calls[result.validatedIndex];
 
             // mistake is inside the pipeline => validation message on wrong call
             if (call) {
+                const validationMessage = computeValidationMessage(result)
                 accept('warning',
                     validationMessage, {
                         node: call,
@@ -49,9 +50,12 @@ export const pipelineMustFollowBehaviourProtocol = (services: SafeDsServices) =>
                 );
             }
             // something is missing at the end of the pipeline => validation message on the end of the pipeline
+            // TODO: generate different message to better explain what is missing
+            //      "after this statement continuation of previous phase or next phase expected"
             else {
+                const specialValidationMessage = computeValidationMessage(result)
                 accept('warning',
-                    validationMessage, {
+                    specialValidationMessage, {
                         node: calls.at(calls.length-1) ?? node,
                         code: CODE_PIPELINE_BEHAVIOUR_PROTOCOL,
                     },
@@ -76,13 +80,9 @@ export const pipelineMustFollowBehaviourProtocol = (services: SafeDsServices) =>
  * has been found. therefore the calls after the violation can not be mapped to a pipeline layer.
  * 
  * also, evaluation and testing only differ in the set used so how does the protocol differentiate?
- */
+*/
 
 
-// possibly different functions for every case?
-//      exploration on validation/test data during processing
-//      evaluation on training/test data during evaluation
-//      evaluation on training/validation data during testing
 export const pipelineMustNotAccessDatasetInWrongPhase = (services: SafeDsServices) => {
     const nodeMapper = services.helpers.NodeMapper;
     const builtinAnnotations = services.builtins.Annotations;
@@ -98,11 +98,12 @@ export const pipelineMustNotAccessDatasetInWrongPhase = (services: SafeDsService
 const computeValidationMessage = (result: ValidationResult): string => {
     const nestedErrors = extractNestedValidationErrors(result);
     const messages: string[] = [];
+    let phase: string = '';
 
     for (const error of nestedErrors){
         switch (error.type){
             case 'elem-block-phase-mismatch': {
-                messages.push(`Expected phase '${error.expected.name}' but found '${error.found.name}.'`);
+                messages.push(`Expected activity '${error.expected.activityName}' but found '${error.found.activityName}.'`);
                 break;
             }
             case 'elem-block-oob': {
@@ -110,25 +111,36 @@ const computeValidationMessage = (result: ValidationResult): string => {
                 break;
             }
             case 'alternative-block-no-match': {
-                messages.push('None of the allowed phases matched.');
+                messages.push('None of the allowed activities in matched.');
                 break;
             }
             case 'or-block-no-match': {
                 const expectedPhasesString = [] as string[];
                 for (const alternative of error.alternatives){
-                    expectedPhasesString.push(alternative.name);
+                    expectedPhasesString.push(alternative.activityName);
                 }
                 console.log(expectedPhasesString);
-                messages.push('Expected one of the following phases but found none: ' + expectedPhasesString.map(p => `'${p}'`).join(', '));
+                // use phase name if possible
+                if (phase != ''){
+                    messages.push(`Expected one of the following activities during phase ${phase} but found none: ` + expectedPhasesString.map(p => `'${p}'`).join(', ') + '.');
+                }
+                // use generic phrase otherwise
+                else {
+                    messages.push('Expected one of the following activities during current phase but found none: ' + expectedPhasesString.map(p => `'${p}'`).join(', ') + '.');
+                }
                 break;
             }
             case 'xor-block-multiple-matches': {
-                messages.push('Multiple exclusive phases were found (expected exactly one).');
+                messages.push('Multiple exclusive activities were found (expected exactly one).');
                 break;
             }
             case 'repetition-block-minimum-not-met': {
-                const blockName = error.name ? ` '${error.name}'` : '';
-                messages.push(`Block${blockName} requires at least ${error.min} occurrences but found ${error.actual}.`);
+                if (error.phaseName){ phase = `'` + error.phaseName + `'`; }
+                
+                // dont show add message if minimum is less than 1 or no activity detected
+                if (error.min > 1 && error.actual > 1){
+                    messages.push(`Detected phase ${phase} requires at least ${error.min} occurrences but found ${error.actual}.`);
+                }
                 break;
             }
             case 'sequence-block-failed': {  
@@ -136,7 +148,7 @@ const computeValidationMessage = (result: ValidationResult): string => {
             }
         }
     }
-    return messages.join('; ');
+    return messages.join(' ');
 }
 
 const extractNestedValidationErrors = (result: ValidationResult): ValidationError[] => {
@@ -160,14 +172,14 @@ const behaviourProtocol = new SequenceBlock([
 // Pre-Processing Layer
     // Data Acquisition
     new RepetitionBlock(
-        new ElementaryBlock( new Phase('DataAcquisition') ),
+        new ElementaryBlock( new Activity('DataAcquisition') ),
         'DataAcquisition', 1
     ),
     new RepetitionBlock(
         new AlternativeBlock([
-            new ElementaryBlock( new Phase('Preprocessing') ),
-            new ElementaryBlock( new Phase('DataAcquisition') ),
-            new ElementaryBlock( new Phase('AcquisitionAndEngineering') )],
+            new ElementaryBlock( new Activity('Preprocessing') ),
+            new ElementaryBlock( new Activity('DataAcquisition') ),
+            new ElementaryBlock( new Activity('AcquisitionAndEngineering') )],
             'or'
         ),  'DataAcquisition'
     ),
@@ -175,29 +187,29 @@ const behaviourProtocol = new SequenceBlock([
     // Data Preparation
     new RepetitionBlock(
         new AlternativeBlock([
-            new ElementaryBlock( new Phase('DataPreparation') ),
-            new ElementaryBlock( new Phase('Exploration') ),
-            new ElementaryBlock( new Phase('Preprocessing') ),
-            new ElementaryBlock( new Phase('PreparationAndEngineering') ),
-            new ElementaryBlock( new Phase('PreparationProcessingAndEngineering') )],
+            new ElementaryBlock( new Activity('DataPreparation') ),
+            new ElementaryBlock( new Activity('Exploration') ),
+            new ElementaryBlock( new Activity('Preprocessing') ),
+            new ElementaryBlock( new Activity('PreparationAndEngineering') ),
+            new ElementaryBlock( new Activity('PreparationProcessingAndEngineering') )],
             'or'
         ),  'DataPreparation'
     ),
 
     // Data Partioning
     new RepetitionBlock(
-        new ElementaryBlock( new Phase('DataPartitioning') ),
+        new ElementaryBlock( new Activity('DataPartitioning') ),
         'DataPartitioning', 1
     ),
 
     // Data Processing
     new RepetitionBlock(
         new AlternativeBlock([
-            new ElementaryBlock( new Phase('DataProcessing') ),
-            new ElementaryBlock( new Phase('DataTransformer') ),
-            new ElementaryBlock( new Phase('Exploration') ),
-            new ElementaryBlock( new Phase('PreparationAndProcessing') ),
-            new ElementaryBlock( new Phase('PreparationProcessingAndEngineering') )], 
+            new ElementaryBlock( new Activity('DataProcessing') ),
+            new ElementaryBlock( new Activity('DataTransformer') ),
+            new ElementaryBlock( new Activity('Exploration') ),
+            new ElementaryBlock( new Activity('PreparationAndProcessing') ),
+            new ElementaryBlock( new Activity('PreparationProcessingAndEngineering') )], 
             'or'
         ), 'DataProcessing'
     ),
@@ -206,28 +218,28 @@ const behaviourProtocol = new SequenceBlock([
     // Feature Engineering
     new RepetitionBlock(
         new AlternativeBlock([
-            new ElementaryBlock( new Phase('FeatureEngineering') ),
-            new ElementaryBlock( new Phase('FeatureTransformer') ),
-            new ElementaryBlock( new Phase('Exploration') ),
-            new ElementaryBlock( new Phase('AcquisitionAndEngineering') ),
-            new ElementaryBlock( new Phase('PreparationProcessingAndEngineering') )], 
+            new ElementaryBlock( new Activity('FeatureEngineering') ),
+            new ElementaryBlock( new Activity('FeatureTransformer') ),
+            new ElementaryBlock( new Activity('Exploration') ),
+            new ElementaryBlock( new Activity('AcquisitionAndEngineering') ),
+            new ElementaryBlock( new Activity('PreparationProcessingAndEngineering') )], 
             'or'
         ), 'FeatureEngineering'
     ),
 
     // Feature Selection
     new RepetitionBlock(
-        new ElementaryBlock( new Phase('FeatureSelection') ),
+        new ElementaryBlock( new Activity('FeatureSelection') ),
         'FeatureSelection'
     ),
 
     // Modeling
     new RepetitionBlock(
         new AlternativeBlock([
-            new ElementaryBlock( new Phase('Modeling') ),
-            new ElementaryBlock( new Phase('ModelingQClassification') ),
-            new ElementaryBlock( new Phase('ModelingQRegression') ),
-            new ElementaryBlock( new Phase('ModelingQNeuralNetwork') )],
+            new ElementaryBlock( new Activity('Modeling') ),
+            new ElementaryBlock( new Activity('ModelingQClassification') ),
+            new ElementaryBlock( new Activity('ModelingQRegression') ),
+            new ElementaryBlock( new Activity('ModelingQNeuralNetwork') )],
             'or'
         ),  'Modeling', 1
     ),
@@ -235,39 +247,39 @@ const behaviourProtocol = new SequenceBlock([
     // Training
     new RepetitionBlock(
         new AlternativeBlock([
-            new ElementaryBlock( new Phase('Training') ),
-            new ElementaryBlock( new Phase('TrainingQClassification') ),
-            new ElementaryBlock( new Phase('TrainingQRegression') ),
-            new ElementaryBlock( new Phase('TrainingQNeuralNetwork') )],
+            new ElementaryBlock( new Activity('Training') ),
+            new ElementaryBlock( new Activity('TrainingQClassification') ),
+            new ElementaryBlock( new Activity('TrainingQRegression') ),
+            new ElementaryBlock( new Activity('TrainingQNeuralNetwork') )],
             'or'
         ),  'Training', 1
     ),
 
     // Prediction
     new RepetitionBlock(
-        new ElementaryBlock( new Phase('Prediction') ),
+        new ElementaryBlock( new Activity('Prediction') ),
         'Prediction'
     ),
 
     // Evaluation
     new RepetitionBlock(
         new AlternativeBlock([
-            new ElementaryBlock( new Phase('EvaluationQMetric') ),
-            new ElementaryBlock( new Phase('EvaluationQVisualization') )],
+            new ElementaryBlock( new Activity('EvaluationQMetric') ),
+            new ElementaryBlock( new Activity('EvaluationQVisualization') )],
             'or'
         ),  'Evaluation', 1
     ),
 
     // Testing
     new RepetitionBlock(
-        new ElementaryBlock( new Phase('EvaluationQMetric') ),
+        new ElementaryBlock( new Activity('EvaluationQMetric') ),
         'Testing', 1
     ),
 
 // Post-Processing Layer
     // Interpretation
     new RepetitionBlock(
-        new ElementaryBlock( new Phase('EvaluationQVisualization') ),
+        new ElementaryBlock( new Activity('EvaluationQVisualization') ),
         'Interpretation', 1
     ),
 ])
