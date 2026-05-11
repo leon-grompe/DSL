b0b70d05dfc27a621e8f82ca0e6a1e36c655cb2a
Leon Grompe <leon.grompe@gmail.com>
2026-05-11 14:16:03 +0200

    new file for suggested pipeline structure

---
+++ packages/safe-ds-lang/src/language/validation/pipeline/pipelineStructure.ts
@@ -0,0 +1,24 @@
+import { ValidationAcceptor } from 'langium';
+import { SdsPipeline } from '../../generated/ast.js';
+import { SafeDsServices } from '../../index.js';
+
+export const CODE_SUGGEST_PIPELINE_STRUCTURE = 'pipeline/pipeline-suggestion'
+
+export const suggestPipelineStructure = (services: SafeDsServices) => {
+    const locator = services.workspace.AstNodeLocator;
+    return (node : SdsPipeline, accept: ValidationAcceptor) => {
+        // check if pipeline is empty (no statements and no comments)
+        if (!/(pipeline\s+\w+\s*\{)(\s*)(\})/.test(node.$cstNode?.text ?? '') || node.body.statements.length > 0){ 
+            return; 
+        }
+        
+        accept('info', 
+            'Pipeline is empty. Suggestion for Pipeline structure available.', {   
+                node: node,
+                code: CODE_SUGGEST_PIPELINE_STRUCTURE,
+                data: { path: locator.getAstNodePath(node) }
+            }
+        );
+        return;
+    }
+}
