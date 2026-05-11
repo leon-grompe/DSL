3fdcdbb878feb2b934fbe90c42a383e6ca0a45e2
Leon Grompe <leon.grompe@gmail.com>
2026-05-11 14:09:23 +0200

    updated import for new file

--- packages/safe-ds-lang/src/language/validation/safe-ds-validator.ts
+++ packages/safe-ds-lang/src/language/validation/safe-ds-validator.ts
@@ -189,7 +189,8 @@ import {
 import { messageOfConstraintsMustOnlyReferenceConstantParameters } from './other/declarations/constraints.js';
 import { argumentMustBeNamedIfParameterIsOptional } from './other/expressions/arguments.js';
 import { typeMustBeUsedInCorrectContext } from './other/types/types.js';
-import { pipelineMustFollowBehaviourProtocol, suggestPipelineStructure } from './pipeline/behaviourProtocol.js';
+import { pipelineMustFollowBehaviourProtocol } from './pipeline/behaviourProtocol.js';
+import { suggestPipelineStructure } from './pipeline/pipelineStructure.js'
 
 import { testDataUsedForTraining } from './data-flow-analysis/datasetUsage.js';
 
