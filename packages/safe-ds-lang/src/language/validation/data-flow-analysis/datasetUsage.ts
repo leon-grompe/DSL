ecf111974b7e15fb4e864507567fe19c29ffe7f1
Leon Grompe <leon.grompe@gmail.com>
2026-05-30 23:28:52 +0200

    better validation message for segment calls

--- packages/safe-ds-lang/src/language/validation/data-flow-analysis/datasetUsage.ts
+++ packages/safe-ds-lang/src/language/validation/data-flow-analysis/datasetUsage.ts
@@ -1,6 +1,6 @@
 import { ValidationAcceptor } from 'langium';
 import { SafeDsServices } from '../../safe-ds-module.js';
-import { SdsPlaceholder, SdsCall, SdsPipeline, isSdsReference, isSdsPlaceholder, isSdsAssignment } from '../../generated/ast.js';
+import { SdsPlaceholder, SdsCall, SdsPipeline, isSdsReference, isSdsPlaceholder, isSdsAssignment, SdsLocalVariable, isSdsLocalVariable, isSdsSegment } from '../../generated/ast.js';
 
 export const CODE_TEST_DATA_USED_FOR_TRAINING = 'data-flow-analysis/test-data-used-for-training';
 
@@ -8,6 +8,7 @@ export const CODE_TEST_DATA_USED_FOR_TRAINING = 'data-flow-analysis/test-data-us
 export const testDataUsedForTraining = (services: SafeDsServices) => {
     const analyzer = services.flow.DataFlowAnalyzer;
     const locator = services.workspace.AstNodeLocator;
+    const nodeMapper = services.helpers.NodeMapper;
 
     return (node: SdsPipeline, accept: ValidationAcceptor) => {
         const pipelineStatements = node.body.statements;
@@ -22,12 +23,15 @@ export const testDataUsedForTraining = (services: SafeDsServices) => {
 
         // Determine placeholder to compute forward slice from
         const trainingSetPlaceholder = splitAssignments[0]?.assigneeList?.assignees[0];
+        const trainingSetName = trainingSetPlaceholder?.$cstNode?.text.slice(4)
 
         // Compute all forward references of the training set
-        const forwardVariables = services.flow.Slicer.computeForwardSliceFromVariable(trainingSetPlaceholder as SdsPlaceholder);
+        const forwardVariables = services.flow.Slicer.computeForwardSliceFromVariable(trainingSetPlaceholder as SdsLocalVariable);
 
         for (const call of fitCalls) {
             const argumentArray = call.argumentList.arguments;
+            const callable = nodeMapper.callToCallable(call);
+            
             for (const argument of argumentArray) {
                 if (!isSdsReference(argument.value)) continue;
                 const argRef = argument.value.target.ref;
@@ -39,8 +43,14 @@ export const testDataUsedForTraining = (services: SafeDsServices) => {
                 if (forwardVariables.some(variable => variable === argRef)) {
                     continue;
                 } else {
+                    let message: string = ``;
+                    if (isSdsSegment(callable)) {
+                        message = `This segment makes use of a '.fit()' call wich does not use a dataset derived from the training set ('${trainingSetName}').`
+                    } else {
+                        message = `Only the training dataset ('${trainingSetName}') should be used for fitting.`;
+                    }
                     accept('warning',
-                        `Only the training dataset ('${argument.$cstNode?.text}') should be used for fitting.`, {
+                        message, {
                         node: argument,
                         property: 'value',
                         code: CODE_TEST_DATA_USED_FOR_TRAINING,
