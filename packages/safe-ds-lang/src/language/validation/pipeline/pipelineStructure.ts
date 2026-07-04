1f8601e4948969172b93aac38ef403461e115a4f
Leon Grompe <leon.grompe@gmail.com>
2026-07-04 11:21:13 +0200

    fix: early return when pipeline has no body and better wording

--- packages/safe-ds-lang/src/language/validation/pipeline/pipelineStructure.ts
+++ packages/safe-ds-lang/src/language/validation/pipeline/pipelineStructure.ts
@@ -8,12 +8,13 @@ export const suggestPipelineStructure = (services: SafeDsServices) => {
     const locator = services.workspace.AstNodeLocator;
     return (node : SdsPipeline, accept: ValidationAcceptor) => {
         // check if pipeline is empty (no statements and no comments)
+        if (!node.body) return;
         if (!/(pipeline\s+\w+\s*\{)(\s*)(\})/.test(node.$cstNode?.text ?? '') || node.body.statements.length > 0){ 
             return; 
         }
         
         accept('info',
-            'This pipeline is empty. A quick fix is available to insert a suggested data-science pipeline structure.', {
+            'This pipeline is empty. Get started by inserting the recommended data-science pipeline structure.', {
                 node: node, 
                 property: 'name',
                 code: CODE_SUGGEST_PIPELINE_STRUCTURE,
